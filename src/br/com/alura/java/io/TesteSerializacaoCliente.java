01d46795e996ee24b18497a5fea6ea68c146adcb
Jose Vitor <jose.vitor@accenture.com>
2022-04-19 17:46:25 -0300

    Creating an object serialized and deserializing it

---
+++ src/br/com/alura/java/io/TesteSerializacaoCliente.java
@@ -0,0 +1,29 @@
+package br.com.alura.java.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class TesteSerializacaoCliente {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+//		Cliente cliente = new Cliente();
+//		cliente.setNome("Vitor");
+//		cliente.setProfissao("Dev");
+//		cliente.setCpf("23432423432");
+//		
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+//		oos.writeObject(cliente);
+//		oos.close();
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
+		Cliente cliente = (Cliente) ois.readObject();
+		ois.close();
+		System.out.println(cliente.getCpf());
+		
+	}
+
+}
